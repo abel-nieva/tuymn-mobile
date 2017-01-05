65e807a5807d462124a2aa143aedc4b6ef8546eb
Abel Nieva <abel.nieva.saavedra@gmail.com>
2017-01-05 17:40:46 +0100

    Form validations in signup pages

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,25 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder } from '@angular/forms';
 import { ViewController } from 'ionic-angular';
-
-import Parse from 'parse';
 
 @Component({
   selector: 'page-signup',
   templateUrl: 'signup.html'
 })
-export class SignupPage {
-  private email: string;
-  private password: string;
+export class SignupPage implements OnInit {
+  public signupForm: any;
+  private emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
   constructor(
-    public viewCtrl: ViewController
+    public viewCtrl: ViewController,
+    private formBuilder: FormBuilder
   ) {}
+
+  ngOnInit() {
+    this.signupForm = this.formBuilder.group({
+      email: ['', Validators.required, Validators.pattern(this.emailRegex)],
+      password: ['', Validators.required]
+    });
+  }
+
+  logForm(){
+    console.log(this.signupForm.value)
+  }
 
   dismiss() {
     this.viewCtrl.dismiss();
-  }
-
-  signup() {
   }
 
 }
